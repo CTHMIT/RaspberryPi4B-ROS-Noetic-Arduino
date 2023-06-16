b488c015147ce8cbbe41bbb7e7d499da675fdd69
C.T. Hsu <107465888+CTHMIT@users.noreply.github.com>
2023-06-16 13:52:55 +0800

    Create cmd_checker.py

---
+++ RasberryPi4B_Arduino/ArduinoCar_RaspberryPiROS/cmd_checker.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo("I heard Raspberry Pi talk to Arduino %s", data.data)
+    
+def checker():
+    rospy.init_node('cmd_checker', anonymous=True)
+    rospy.Subscriber("feedback", String, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    checker()
